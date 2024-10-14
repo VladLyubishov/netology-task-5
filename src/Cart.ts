42fd2e8c7be80b82fcf7741ee1319f994421e7c3
Любишов Влад <LubishovV@mik-inform.ru>
2024-10-14 15:22:21 +0300

    Исправление замечаний

--- src/Cart.ts
+++ src/Cart.ts
@@ -12,19 +12,15 @@ export default class Cart{
     }
 
     getPrice(): number{
-        let sum = this._items.reduce((initial, item) => initial + item.price, 0);
-        return sum;
+        return this._items.reduce((initial, item) => initial + item.price, 0);
     }
 
     priceWithDiscount(discount: number): number{
-        let sum = this._items.reduce((initial, item) => initial + item.price, 0);
+        const sum = this._items.reduce((initial, item) => initial + item.price, 0);
         return sum - (sum * (discount / 100));
     }
 
     removeFromCart(id: number): void{ 
-        const index = this._items.findIndex(item => item.id === id);
-        if (index !== -1) {
-            this._items.splice(index, 1);
-        }
+        this._items = this._items.filter(item => item.id !== id);
     }
 }
